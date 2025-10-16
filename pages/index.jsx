8eb681be58a55c26e69a911198cd3cef07b25bd2
Fred Hoze <fred.hoze@gmail.com>
2025-10-16 20:47:36 +0200

    Nouveau départ sans node_modules

---
+++ pages/index.jsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-purple-100">
+      <h1 className="text-3xl font-bold text-purple-800">Bienvenue dans Ma Cave à Vin 🍷</h1>
+    </div>
+  );
+}
